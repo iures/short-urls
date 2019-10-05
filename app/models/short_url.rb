6f13f118939a3e21b99d4cde492b43c90305238f
Iure Sales <iures@crowdtap.com>
2019-10-05 18:20:21 -0400

    Adds full_url url validator to the short_urls domain
    This will enforce that the submitted url is a valid one
    before creating a short_url instance

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,14 +2,17 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
-  validate :validate_full_url
+  validates :full_url, :presence => true, :url => true
 
-  def update_title!
+  before_create :generate_short_code
+
+  def generate_short_code
+    self.short_code = CHARACTERS.sample(3) #temporary
   end
 
   private
 
-  def validate_full_url
+  def update_title!
   end
 
 end
