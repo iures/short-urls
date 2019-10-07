836fe7b19c2e17f923a94fab4750c780fbc3457b
Iure Sales <iures@crowdtap.com>
2019-10-07 17:19:14 -0400

    ShorCodeParser now deals with invalid numbers and codes when parsing

--- lib/short_code_parser.rb
+++ lib/short_code_parser.rb
@@ -3,6 +3,10 @@ class ShortCodeParser
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
   def self.from_number(number)
+    if number <= 0
+      raise ArgumentError.new('invalid number')
+    end
+
     value = ''
 
     if number / CHARACTERS.length > 0
@@ -13,11 +17,21 @@ class ShortCodeParser
   end
 
   def self.to_number(short_code)
+    if short_code.empty?
+      raise ArgumentError.new('invalid code')
+    end
+
     number = 0
 
     short_code.each_char do |char|
+      char_index = CHARACTERS.index(char)
+
+      if char_index.nil?
+        raise ArgumentError.new('invalid code')
+      end
+
       number *= CHARACTERS.length
-      number += CHARACTERS.index(char)
+      number += char_index
     end
 
     number
